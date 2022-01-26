695d32e2dd8b6b72fe423e68253ca56106f5b388
Senthil Palanivelu <senthilcaesar@yahoo.co.in>
2022-01-26 07:00:03 +0530

    Update fieldtrip_ICA.m

--- artifact/fieldtrip_ICA.m
+++ artifact/fieldtrip_ICA.m
@@ -1,11 +1,12 @@
 cfg            = [];
-cfg.dataset    = 'Podcast_Eyes_Closed_2.edf';
+cfg.dataset    = 'eye_open_2.edf';
 cfg.continuous = 'yes';
-cfg.channel    = {'AF3', 'AF4', 'F7', 'F3', 'F4', 'F8', 'FC5', 'FC6'};
+cfg.channel    = {'AF3', 'AF4', 'F7', 'F3', 'F4', 'F8', 'FC5', 'FC6',...
+                   'T7', 'T8', 'P7', 'P8', 'O1', 'O2'};
 data           = ft_preprocessing(cfg);
 
-figure(1)
 before = data.trial{1};
+subplot(2,1,1)
 plot(before(1,:))
 legend('Raw data')
 
@@ -17,11 +18,12 @@ comp = ft_componentanalysis(cfg, data);
 
 % Remove the blink components
 cfg = [];
-cfg.component = [3 4]; % to be removed components
+cfg.component = (2); % to be removed components
 data = ft_rejectcomponent(cfg, comp, data);
 
-figure(2)
+
 after = data.trial{1};
+subplot(2,1,2)
 plot(after(1,:))
 
 legend('ICA filtered')
