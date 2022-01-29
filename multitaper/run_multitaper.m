2c76c0a4893823c80483d316476b9008075c4e56
Senthil Palanivelu <senthilcaesar@yahoo.co.in>
2022-01-29 10:46:25 +0530

    Update run_multitaper.m

--- multitaper/run_multitaper.m
+++ multitaper/run_multitaper.m
@@ -1,10 +1,12 @@
-dataFold='/Users/senthilp/Desktop/lanlan/matlab_code/podcast1/';
-saveFold='/Users/senthilp/Desktop/lanlan/matlab_code/podcast1/spectrogram/';
+input_data = csvread('Podcast_Eye_Closed_2_Chinese.csv',2,0); % This excludes the first two rows
+eegcols = 5:18; % EEG Columns
+eeg.raw = input_data(:,eegcols);
 
-frontal_elec = {'AF3', 'AF4', 'F7', 'F3', 'F4', 'F8', 'FC5', 'FC6'};
-occ_elec = {'O1', 'O2'};
+% AF3
+channel_data = eeg.raw(:,1);
 
-%------------- Parameters---------------------------------------%
+
+%------------- Time frequncy analysis Parameters---------------------------%
 N = 2.5;
 overlap = .05;
 freq_resolution = 4;
@@ -12,7 +14,7 @@ TW = (N * freq_resolution)/2;
 K = floor(2 * TW)-1;
 
 fs = 128;                    % Sampling Frequency
-frequency_range = [0 60];    % Limit frequencies from 0 to 60 Hz
+frequency_range = [0 64];    % Limit frequencies from 0 to 60 Hz
 taper_params = [TW K];       % Time bandwidth and number of tapers
 window_params = [N overlap]; % Window size is 2.5s with step size of .05s
 min_nfft = 0;                % No minimum nfft
@@ -22,18 +24,15 @@ plot_on = true;              % plot spectrogram
 verbose = true;              % print extra info
 %-------------------------------------------------------------%
 
-for elecID=1:length(frontal_elec)
-   
-    elecIS = frontal_elec{elecID};
-    mat_data = [ dataFold '/electrode' elecIS '.mat'];
-    data = load(mat_data);
-    saveData=[saveFold 'electrode_' elecIS '_spectrogram.mat'];
-    data=data.chanData';
-    [spect,stimes,sfreqs] = multitaper_spectrogram(data,fs,frequency_range, taper_params, window_params, min_nfft, detrend_opt, weighting, plot_on, verbose);
-    save(saveData, 'spect','stimes','sfreqs','fs','N','Freq','overlap');
-    
-end
+saveData='lanlan_spectrogram.mat';
+[spect,stimes,sfreqs] = multitaper_spectrogram(channel_data,fs,frequency_range, taper_params, window_params, min_nfft, detrend_opt, weighting, plot_on, verbose);
+save(saveData, 'spect','stimes','sfreqs','fs','N','overlap');
 
+theta_pw = bandpower(spect, sfreqs, [4,8], 'psd');
+plot(stimes, theta_pw)
+title('Real time Theta band power')
+xlabel('Time');
+ylabel('Power');
 
 
 
